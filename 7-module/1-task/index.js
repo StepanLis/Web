fe0967b7ea30ec4c4b173312a71bdffb51989f1a
StepanLis <Stepa-lisitsin@mail.ru>
2023-03-23 15:05:49 +0500

    Изменения в js

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -7,29 +7,26 @@ export default class RibbonMenu {
   #arrowLeft = null;
   #ribbonInner = null;
   #scrollRight = 0;
+  #ribon = null;
   constructor(categories) {
     this.#categories = categories;
     this.#render();
     this.#arrowLeft = this.elem.querySelector('.ribbon__arrow_left');
     this.#arrowRight = this.elem.querySelector('.ribbon__arrow_right');
     this.#ribbonInner = this.elem.querySelector('.ribbon__inner');
+    this.#ribon = this.elem.closest('.ribbon');
 
     this.elem.querySelector('.ribbon__item').classList.add('ribbon__item_active');
-    this.#arrowLeft.classList.remove('ribbon__arrow_visible');
-    this.#arrowRight.classList.add('ribbon__arrow_visible');
-
-    this.#arrowLeft.addEventListener('click', this.#clickArrowLeft);
-    this.#arrowRight.addEventListener('click', this.#clickArrowRight);
 
     this.#ribbonInner.addEventListener('scroll', this.#visibleArrow);
-    this.#ribbonInner.addEventListener('click', this.#onButtonClick);
+    this.#ribon.addEventListener('click', this.#onClick);
   }
-// ribbon__item_active
+
   #render(){
     this.elem = createElement(`
       <div class="ribbon">
         <!--Кнопка прокрутки влево-->
-        <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+        <button class="ribbon__arrow ribbon__arrow_left">
           <img src="/assets/images/icons/angle-icon.svg" alt="icon">
         </button>
 
@@ -42,20 +39,32 @@ export default class RibbonMenu {
         </nav>
 
         <!--Кнопка прокрутки вправо-->
-        <button class="ribbon__arrow ribbon__arrow_right">
+        <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
           <img src="/assets/images/icons/angle-icon.svg" alt="icon">
         </button>
       </div>
     `)
   }
 
-  #clickArrowLeft = () =>{
-    this.#ribbonInner.scrollBy(-350, 0);
-  };
 
-  #clickArrowRight = () => {
-    this.#ribbonInner.scrollBy(350, 0);
-  };
+  #onClick = (event) =>{
+    if(event.target.classList.contains('ribbon__arrow_left')){
+      this.#ribbonInner.scrollBy(-350, 0);
+    }else if (event.target.classList.contains('ribbon__arrow_right')){
+      this.#ribbonInner.scrollBy(350, 0);
+    }else if(event.target.closest('.ribbon__item')){
+      event.preventDefault();
+      this.elem.querySelector('.ribbon__item_active').classList.remove('ribbon__item_active');
+      event.target.classList.add('ribbon__item_active');
+      
+      this.elem.dispatchEvent(
+        new CustomEvent('ribbon-select', {
+          detail: event.target.closest('.ribbon__item').dataset.id, 
+          bubbles: true
+        })
+      );
+    }
+  }
 
   #visibleArrow = () => {
     this.#scrollRight = this.#ribbonInner.scrollWidth - this.#ribbonInner.scrollLeft - this.#ribbonInner.clientWidth;
@@ -68,22 +77,6 @@ export default class RibbonMenu {
       this.#arrowLeft.classList.remove('ribbon__arrow_visible');
     }else{
       this.#arrowLeft.classList.add('ribbon__arrow_visible');
-    }
-  } 
-
-  #onButtonClick = (event) =>{
-    event.preventDefault();
-
-    if(event.target.closest('.ribbon__item')){
-      this.elem.querySelector('.ribbon__item_active').classList.remove('ribbon__item_active');
-      event.target.classList.add('ribbon__item_active');
-      
-      this.elem.dispatchEvent(
-        new CustomEvent('ribbon-select', {
-          detail: event.target.closest('.ribbon__item').dataset.id, 
-          bubbles: true
-        })
-      );
     }
   }
 
